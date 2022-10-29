e4413d2b5f4267973fc1bd3ebd877649ba867825
Luo-JianG <2434559682@qq.com>
2022-10-29 13:28:19 +0800

    feat:完善用户列表

--- src/api/index.js
+++ src/api/index.js
@@ -4,3 +4,20 @@ import http from '../utils/request'
 export const getData = () => {
     return http.get('/home/getData') //返回一个promise对象
 }
+
+//请求用户数据
+export const getUser = (params) => {
+    return http.get('/home/getUserList',params)
+}
+
+export const addUser = (data) => {
+    return http.post('/home/addUser',data)
+}
+
+export const delUser = (data) => {
+    return http.post('/home/delUser',data)
+}
+
+export const editUser = (data) => {       
+    return http.post('/home/editUser',data)
+}
